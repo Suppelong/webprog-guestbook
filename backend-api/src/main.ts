9e1c29f93cc1ed42f434bf8ed226036841f43629
Your Name <bacarpio@student.apc.edu.ph>
2026-02-16 17:07:28 +0800

    fix: allow all cors origins to fix connection issues

--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -5,12 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   // This line is essential for the Frontend to talk to the Backend!
-app.enableCors({
-  origin: [
-    'http://localhost:5173',
-    'https://webprog-guestbook.vercel.app' // 👈 Make sure this matches your Vercel link EXACTLY
-  ],
-});
+  app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
 }
