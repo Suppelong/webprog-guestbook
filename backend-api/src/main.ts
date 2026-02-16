460968731c713fc9176e239195ef465ee5318dbf
Lance Art P Fortaleza <lpfortaleza@student.apc.edu.ph>
2026-02-16 16:16:16 +0800

    .

--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -5,7 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   // This line is essential for the Frontend to talk to the Backend!
-  app.enableCors(); 
+app.enableCors({
+  origin: [
+    'http://localhost:5173',
+    'https://webprog-guestbook.vercel.app' // 👈 Make sure this matches your Vercel link EXACTLY
+  ],
+});
 
   await app.listen(3000);
 }
